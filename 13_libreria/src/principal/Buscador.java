62a514411baa979e33d71f567a3c6f486d5a3999
usuario <usuario@macbook-jc.home>
2020-07-07 10:09:40 +0200

    Creando el proyecto nro 13. Commit 1

---
+++ 13_libreria/src/principal/Buscador.java
@@ -0,0 +1,96 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Buscador {
+	static StringBuilder titulos = new StringBuilder("");
+	static Scanner scNros = new Scanner(System.in);
+	static Scanner scTxt = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int selector = 0;
+
+		do {
+			System.out.println("Menú (pulsa número para seleccionar): ");
+			System.out.println("1. Agregar libro");
+			System.out.println("2. Buscar libro");
+			System.out.println("3. Eliminar libro");
+			System.out.println("4. Salir");
+
+			selector = scNros.nextInt();
+
+			switch (selector) {
+			case 1:
+				agregarTitulos(titulos);
+				System.out.println("Has añadido el título. Actualmente están: " + titulos);
+				break;
+			case 2:
+				buscarLibro();
+				break;
+			case 3:
+				borrarLibros();
+				break;
+			default:
+				System.out.println("Has salido.");
+			}
+		} while (selector != 4);
+		System.out.println("Hasta luego!");
+		scNros.close();
+		scTxt.close();
+	}
+
+	static void buscarLibro() {
+		String[] titulosSeparados = titulos.deleteCharAt(titulos.length() - 1).toString().split(", |,");
+
+		System.out.println("Introduce tu búsqueda: ");
+		String textoBuscado = scTxt.nextLine();
+
+		System.out.println("Estos son los libros que resultan de tu búsqueda: ");
+
+		for (String n : titulosSeparados) {
+			if (n.toLowerCase().indexOf(textoBuscado.toLowerCase()) != -1) {
+				System.out.println(n);
+			}
+		}
+
+	}
+
+	static void agregarTitulos(StringBuilder titulos) {
+		System.out.println("Introduce un título para agregar: ");
+		String titulo = scTxt.nextLine();
+		titulos = titulos.append(titulo);
+		titulos = titulos.append(", ");
+	}
+
+
+
+	static void borrarLibros() {
+		String[] titulosSeparados = titulos.
+				deleteCharAt(titulos.length()-1).
+				toString().
+				split(", |,");
+	
+		System.out.println("Introduce la búsqueda de libros a borrar: ");
+		String textoBuscado = scTxt.nextLine();
+	
+		System.out.println("Estos son los libros que borrarás: ");
+	
+		for (String n:titulosSeparados) {	
+			if(n.toLowerCase().indexOf(textoBuscado.toLowerCase())!=-1) {
+				System.out.println("El libro"+ n);
+				int inicio = n.indexOf(textoBuscado);
+				int end = inicio+textoBuscado.length();
+				System.out.println("¿Deseas borrar este libro? S / N");
+				if (scTxt.nextLine().equalsIgnoreCase("S")) {
+					titulos.delete(inicio, end);
+					System.out.println("Has borrado el libro "+n);
+				} else {
+					System.out.println("No borrarás este libro.");
+				}
+			}		
+		}
+		
+		
+	}
+	
+}
